afaa658c682019f258554e337269df75691ec783
418098 <115142584+418098@users.noreply.github.com>
2022-11-10 16:46:13 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,24 @@
 let y = 0
 let dx = 1
 let x = 0
+let x2 = 0
+let x3 = 0
 basic.forever(function () {
     y = 0
     if (x == 0) {
         dx = 1
+        x2 = x + 1
+        x3 = x + 2
     } else if (x == 4) {
         dx = -1
+        x2 = x - 1
+        x3 = x - 2
     }
     basic.clearScreen()
     x += dx
     for (let index = 0; index < 5; index++) {
+        led.plotBrightness(x3, y, 20)
+        led.plotBrightness(x2, y, 60)
         led.plot(x, y)
         y += 1
     }
